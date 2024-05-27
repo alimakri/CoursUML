b5414f794a76861a166114b35d7511d16c9d551d
Stéphane RAVET <ravet.st@gmail.com>
2024-05-27 10:33:42 +0200

    Ajoutez des fichiers projet.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using ConsoleApp1.Niveau2;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(Personne1.Nom);
+            Console.WriteLine(Personne1.Prenom);
+            Console.WriteLine(Personne1.Age);
+
+            Console.WriteLine(Personne2.Nom);
+            Console.WriteLine(Personne2.Prenom);
+            Console.WriteLine(Personne2.Age);
+
+        }
+    }
+    namespace Niveau2
+    {
+        public static class Personne1
+        {
+            public static string Nom = "Dupont";
+            public static string Prenom = "Pierre";
+            public static int Age = 25;
+
+        }
+        public static class Personne2
+        {
+            public static string Nom = "Durand";
+            public static string Prenom = "Paul";
+            public static int Age = 35;
+
+        }
+    }
+    namespace Niveau3
+    {
+        namespace Niveau4
+        {
+            class Program
+            {
+                public static void Main2()
+                {
+                    Console.WriteLine("Hello 2");
+                }
+            }
+        }
+    }
+}
