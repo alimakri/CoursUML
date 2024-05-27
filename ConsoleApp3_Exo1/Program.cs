c2941ea89ba8123284b3e1c9d082be061dbc2bc7
Stéphane RAVET <ravet.st@gmail.com>
2024-05-27 12:04:03 +0200

    12:03

---
+++ ConsoleApp3_Exo1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Instancier 3 objets Maison :
+// une bleue avec 2 portes d'entrée code1 123 code2 456,
+// une rouge avec 1 portes d'entrée code 789
+// une verte avec 1 portes d'entrée code 000
+// Définir une propriété indiquant le nombre de maisons instanciées
+
+
+
