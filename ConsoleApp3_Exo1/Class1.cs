b15c2769f23d45ae1a5500d9aebfcb8b99a86036
Stéphane RAVET <ravet.st@gmail.com>
2024-05-27 13:08:02 +0200

    13:07

---
+++ ConsoleApp3_Exo1/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3_Exo1
+{
+    public class Maison
+    {
+        public string Couleur = "Blanc";
+        public Porte Porte1 = null;
+        public Porte Porte2;
+        public static int Nombre = 0;
+    }
+    public class Porte
+    {
+        public string Code = "000";
+    }
+}
--- ConsoleApp3_Exo1/Program.cs
+++ ConsoleApp3_Exo1/Program.cs
@@ -4,5 +4,27 @@
 // une verte avec 1 portes d'entrée code 000
 // Définir une propriété indiquant le nombre de maisons instanciées
 
+using ConsoleApp3_Exo1;
 
+Maison maison1 = new Maison();
+maison1.Couleur = "Bleu";
+maison1.Porte1 = new Porte();   
+maison1.Porte2 = new Porte();
+maison1.Porte1.Code = "123";
+maison1.Porte2.Code = "456";
+Maison.Nombre++;
+
+Maison maison2 = new Maison();
+maison2.Couleur = "Rouge";
+maison2.Porte1 = new Porte();
+maison2.Porte1.Code = "789";
+Maison.Nombre++;
+
+Maison maison3 = new Maison();
+maison3.Couleur = "Vert";
+maison3.Porte1 = new Porte();
+maison3.Porte1.Code = "000";
+Maison.Nombre++;
+
+Console.WriteLine("il y a {0} maison(s)", Maison.Nombre);
 
