75200cf2c145a0e6a7d092bd86a0d92e5098bbc3
Stéphane RAVET <ravet.st@gmail.com>
2024-05-27 14:06:27 +0200

    14:06

--- ConsoleApp4/Class1.cs
+++ ConsoleApp4/Class1.cs
@@ -3,6 +3,7 @@ namespace ConsoleApp4
 {
     public class CoffreFort
     {
-
+        public string Utilisateur = "Inconnu";
+        public string Code = "0000";
     }
 }
--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿using ConsoleApp4;
+
+var coffre1 = new CoffreFort();
+coffre1.Code = "Po45FT";
+
+Console.WriteLine(coffre1.Code);
