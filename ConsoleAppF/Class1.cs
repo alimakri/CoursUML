9530eab0dc8b84ead7fbd5b6ff14b1e7ea7dc891
Ali MAKRI <github@makri.fr>
2024-05-29 12:32:03 +0200

    12:31

---
+++ ConsoleAppF/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppF
+{
+    internal class ClassLourde : IDisposable
+    {
+        public void Dispose()
+        {
+        }
+    }
+}
