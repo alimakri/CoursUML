2d882ee4d16ec3f72122f4e8e64360787c29ca37
Ali MAKRI <github@makri.fr>
2024-05-29 11:50:28 +0200

    11:50

---
+++ ConsoleAppE/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppE
+{
+    public static class MethodesTexte : object
+    {
+        public static int NombreMots(this string s)
+        {
+            return s.Split(' ').Length;
+        }
+        public static int NombreMots(this StringBuilder s)
+        {
+            return s.ToString().NombreMots();
+        }
+    }
+}
---
+++ ConsoleAppE/Program.cs
@@ -0,0 +1,12 @@
+﻿using ConsoleAppE;
+using System.Text;
+
+StringBuilder phrase = new StringBuilder( "8 threads plus de threads de processeur");
+int nCar = phrase.Length;
+int nMots = phrase.NombreMots();
+
+//int nMots = Class1.NombreMots(phrase);
+
+
+Console.WriteLine(nMots);
+Console.ReadLine();
